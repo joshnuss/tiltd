60105cd9fdfa6bb1b0b609c15370bb3b9f67ead4
Josh Nussbaum <joshnuss@gmail.com>
2013-07-09 02:50:45 -0400

    Adds content locate logic to rack application

--- lib/tiltd/application.rb
+++ lib/tiltd/application.rb
@@ -1,16 +1,28 @@
 module Tiltd
   class Application
     def call(env)
-      code = 200
-      mime = 'text/html'
-      body = ''
+      path = find_path(env)
 
-      headers = {
-        'Content-Type'  => mime,
-        'Cache-Control' => 'public, max-age=86400'
-      }
+      if content = Content.locate(path)
+        headers = {
+          'Content-Type'  => content.content_type,
+          'Cache-Control' => 'public, max-age=86400'
+        }
+        code = 200
+        body = content.body
+      else
+        code    = 404
+        headers = {}
+        body    = ''
+      end
 
       [ code, headers, [body] ]
+    end
+
+  private
+    def find_path(env)
+      request = Rack::Request.new(env)
+      request.path_info[1..-1]
     end
   end
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -3,11 +3,30 @@ require 'spec_helper'
 describe Tiltd::Application do
   context "#call" do
     let(:app) { Tiltd::Application.new }
+    let(:request) { stub(path_info: '/foo/other') }
     let(:response) { app.call(:env) }
 
-    specify { response[0].should == 200 }
-    specify { response[1]['Content-Type'].should == 'text/html' }
-    specify { response[1]['Cache-Control'].should == 'public, max-age=86400' }
-    specify { response[2].should == ['']}
+    before do
+      Rack::Request.should_receive(:new).with(:env).and_return(request)
+    end
+
+    context "content found" do
+      let(:content) { stub(body: 'content', content_type: 'text/css') }
+      before { Tiltd::Content.should_receive(:locate).with('foo/other').and_return(content)}
+
+      specify { response[0].should == 200 }
+      specify { response[1]['Content-Type'].should == 'text/css' }
+      specify { response[1]['Cache-Control'].should == 'public, max-age=86400' }
+      specify { response[2].should == ['content']}
+    end
+
+    context "content not found" do
+      before { Tiltd::Content.should_receive(:locate).with('foo/other').and_return(nil) }
+
+      specify { response[0].should == 404 }
+      specify { response[1].should == {} }
+      specify { response[2].should == ['']}
+    end
+
   end
 end
